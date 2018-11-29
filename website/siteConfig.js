c9dfabd549cb9e36f56e237a8e2236f8040388fd
Jake Bolam <jake.bolam@gmail.com>
2018-11-29 11:25:44 -0500

    docs: add analytics

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -20,7 +20,7 @@ const siteConfig = {
         secondaryColor: '#ff8926',
     },
     customDocsPath: 'docs',
-    gaTrackingId: '',
+    gaTrackingId: 'UA-129741728-1',
 
     copyright: 'Top Hat Open Source',
 
