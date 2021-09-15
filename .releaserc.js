184f9188853ba89692ee19b05fa45abd82a2f0fa
mattthaber <matt.haber@tophatmonocle.com>
2021-09-15 12:02:36 -0400

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -10,7 +10,6 @@ module.exports = {
     'plugins': [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
         ['@semantic-release/github', {
             'assets': [
                 { 'path': './release-archive/*'},
