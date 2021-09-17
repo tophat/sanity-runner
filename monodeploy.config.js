27109d01575f724402b9522c7e02577713ca92d2
Matt Haber <matt.haber@tophatmonocle.com>
2021-09-17 14:34:41 -0400

    remove monodeploy

--- monodeploy.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    conventionalChangelogConfig: '@tophat/conventional-changelog-config',
-    noRegistry: true,
-    changelogFilename: '<packageDir>/CHANGELOG.md',
-    persistVersions: true,
-    autoCommitMessage: 'chore: release terraform modules [skip ci]',
-    plugins: ['@monodeploy/plugin-github'],
-}
