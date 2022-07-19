b3ca2de95269641aa7691ac6962f09c3dfb8becc
noahnu <noahnu@gmail.com>
2022-07-18 22:52:39 -0400

    docs: update sanity runner website

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,62 @@
+module.exports = {
+    pathPrefix: '/sanity-runner',
+    assetPrefix: 'https://tophat.github.io',
+    siteMetadata: {
+        title: 'Sanity Runner',
+        description: 'Distributed end to end test runner.',
+        author: 'Top Hat',
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-image',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: `${__dirname}/src/pages/`,
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'gatsby-starter-default',
+                short_name: 'starter',
+                start_url: '/',
+                background_color: '#803ed7',
+                theme_color: '#803ed7',
+                display: 'minimal-ui',
+                icon: 'src/images/logo.png', // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                gatsbyRemarkPlugins: [
+                    {
+                        resolve: require.resolve('gatsby-remark-autolink-headers'),
+                    },
+                    {
+                        resolve: require.resolve('gatsby-remark-images'),
+                        options: { maxWidth: 1000 },
+                    },
+                ],
+                defaultLayouts: {
+                    default: require.resolve('./src/components/Layout.tsx'),
+                },
+            },
+        },
+        'gatsby-plugin-gatsby-cloud',
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+}
---
+++ gatsby/src/components/Layout.tsx
@@ -0,0 +1,64 @@
+import { MDXProvider } from '@mdx-js/react'
+import { graphql, useStaticQuery } from 'gatsby'
+import * as React from 'react'
+import 'sanitize.css'
+
+import CodeBlock from './CodeBlock'
+import Header from './Header'
+import Seo from './Seo'
+import './layout.css'
+
+const components = {
+    code: CodeBlock,
+}
+
+interface PageContext {
+    frontmatter: {
+        path: string
+        title: string
+    }
+}
+
+const Layout: React.FC<{
+    children: React.ReactNode
+    pageContext: PageContext
+}> = ({ children, pageContext }) => {
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
+    return (
+        <MDXProvider components={components}>
+            <Seo title={pageContext?.frontmatter?.title} />
+            <Header siteTitle={data.site.siteMetadata?.title} />
+            <div
+                style={{
+                    margin: '0 auto',
+                    maxWidth: 960,
+                    padding: '0 1.0875rem 1.45rem',
+                }}
+            >
+                <main>{children}</main>
+                <hr />
+                <footer
+                    style={{
+                        marginTop: '2rem',
+                    }}
+                >
+                    <a href="https://opensource.tophat.com/" rel="noreferrer" target="_blank">
+                        Top Hat Open Source
+                    </a>{' '}
+                    © {new Date().getFullYear()}
+                </footer>
+            </div>
+        </MDXProvider>
+    )
+}
+
+export default Layout
