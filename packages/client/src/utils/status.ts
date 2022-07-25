9312b37a832b6394a77c39f7a380f0c09afe979e
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2022-07-25 12:27:10 -0400

    fix: handle errors gracefully

--- packages/client/src/utils/status.ts
+++ packages/client/src/utils/status.ts
@@ -1,17 +1,21 @@
+import type { JUnitReport } from '@tophat/sanity-runner-types'
+
 import { type TestRunResult, TestStatus } from '../types'
 
 export function parseStatus(result: TestRunResult): TestStatus {
-    const testResults = Object.values(result.result?.testResults ?? {})[0]
+    const testResults: JUnitReport | undefined = Object.values(result.result?.testResults ?? {})[0]
 
-    if (testResults.testsuites.testsuite?.[0].$.skipped) {
-        return TestStatus.Skipped
+    if (testResults?.testsuites) {
+        if (testResults.testsuites.testsuite?.[0].$.skipped) {
+            return TestStatus.Skipped
+        }
+        if (result.result?.passed) {
+            return TestStatus.Passed
+        }
     }
-    if (result.result?.passed) {
-        return TestStatus.Passed
-    }
-    if (result.error) {
+
+    if (!testResults || result.error) {
         return TestStatus.Error
     }
-
     return TestStatus.Failed
 }
